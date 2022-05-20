0a4fea4502d59b7b824a16181a0516d31d33f952
scharfsteina <ava.l.scharfstein.24@dartmouth.edu>
2022-05-19 21:10:58 -0400

    basic web scraping

--- test.R
+++ test.R
@@ -1,1 +1,12 @@
 # TEST
+library(rvest)
+
+stocks <- c("GOOG","MSFT","CVS")
+
+for (s in stocks) {
+  html <- read_html(paste0("https://finance.yahoo.com/quote/",s,"/history"))
+  data <- html_table(html)
+  print(data)
+}
+
+
