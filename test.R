4442f728c98f666f7440c295a88d4eb3f6093cfb
scharfsteina <ava.l.scharfstein.24@dartmouth.edu>
2022-05-22 09:50:43 -0400

    data wrangling

--- test.R
+++ test.R
@@ -1,12 +1,36 @@
 # TEST
 library(rvest)
+library(tidyverse)
+library(lubridate)
 
-stocks <- c("GOOG","MSFT","CVS")
+stocks <- html_table(read_html("https://www.dogsofthedow.com/dowcomp.htm"))[[1]]
+stocks <- stocks %>%
+  pull(Symbol)
+  
+# this is only 2022 data
+ex <- html_table(read_html(paste0("https://finance.yahoo.com/quote/",stocks[1],"/history")))[[1]]
+ndays <- lengths(ex)[1]
+data <- data.frame(matrix(nrow = ndays, ncol = 1))
+names(data) <- "date"
 
-for (s in stocks) {
-  html <- read_html(paste0("https://finance.yahoo.com/quote/",s,"/history"))
-  data <- html_table(html)
-  print(data)
+
+for (i in seq_along(stocks)) {
+  html <- read_html(paste0("https://finance.yahoo.com/quote/",stocks[i],"/history"))
+  data <- html_table(html)[[1]] %>% 
+    get_returns() %>% 
+    left_join(data, by = "date")
 }
+names(data) <- c("date",stocks)
+data_cleaned <- data %>%
+  na.omit()
 
-
+get_returns <- function(df) {
+  df %>%
+    mutate(date = as.Date(Date, format = "%b %d, %Y"),
+           close = `Close*` %>% as.double()) %>%
+    filter(!is.na(close)) %>%
+    arrange(date) %>%
+    mutate(percentage = 100*(close-lag(close))/lag(close)) %>% 
+    filter(!is.na(percentage)) %>% 
+    select(date, percentage)
+}
