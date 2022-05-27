abf2e1ee2da47a4b5b2c1c73daa7e0deafd6a7a6
Daniel Carstensen <daniel.l.carstensen.24@dartmouth.edu>
2022-05-27 12:57:40 -0400

    Clean up

---
+++ display_returns.R
@@ -0,0 +1,37 @@
+plot_daily <- function(final) {
+  final %>%
+    pivot_longer(values_to = "returns", names_to = "method", cols = c(markowitz, interval, equal)) %>%
+    ggplot(aes(x = date, y = returns, color = method)) +
+    geom_line() +
+    geom_smooth() +
+    expand_limits(y = c(-1, 1)) +
+    scale_color_manual(labels = c("Markowitz", "Interval", "Equal Weights"), 
+                       values = c("#eb5a0f", "#1c3678", "#249e8f")) +
+    labs(title = "Comparing Returns of Portfolio Allocation Strategies on Dow 30",
+         x = "Date",
+         y = "Return (in %)") +
+    theme_minimal() +
+    theme(plot.title = element_text(hjust = 0.5),
+          legend.title = element_blank(),
+          legend.position = "bottom")
+}
+
+plot_cumulative <- function(final) {
+  final %>%
+    pivot_longer(values_to = "returns", names_to = "method", cols = c(cumulative_markowitz,
+                                                                      cumulative_interval,
+                                                                      cumulative_equal)) %>%
+    ggplot(aes(x = date, y = returns, color = method)) +
+    geom_line() +
+    geom_smooth() +
+    expand_limits(y = c(-1, 1)) +
+    scale_color_manual(labels = c("Markowitz", "Interval", "Equal Weights"), 
+                       values = c("#eb5a0f", "#1c3678", "#249e8f")) +
+    labs(title = "Comparing Returns of Portfolio Allocation Strategies on Dow 30",
+         x = "Date",
+         y = "Return (in %)") +
+    theme_minimal() +
+    theme(plot.title = element_text(hjust = 0.5),
+          legend.title = element_blank(),
+          legend.position = "bottom")
+}
---
+++ linear_combos.R
@@ -0,0 +1,34 @@
+get_linear_combos <- function(data, weights_markowitz, weights_interval) {
+  date <- data[, 1]
+  data <- data[, -1] %>% as.matrix()
+  
+  markowitz <- as.matrix(data) %*% as.matrix(weights_markowitz)
+  interval <- as.matrix(data) %*% as.matrix(weights_interval)
+  equal <- as.matrix(data) %*% as.matrix(rep(1, length(opt_weights)) / length(opt_weights)) # equally weighted
+  
+  cum_markowitz <- markowitz %>% 
+    as_tibble() %>% 
+    mutate(m_plus = V1 + 1,
+           cumulative_markowitz = m_plus %>% cumprod()) %>% 
+    select(cumulative_markowitz)
+  
+  cum_interval <- interval %>% 
+    as_tibble() %>% 
+    mutate(m_plus = V1 + 1,
+           cumulative_interval = i_plus %>% cumprod()) %>% 
+    select(cumulative_interval)
+  
+  cum_equal <- equal %>% 
+    as_tibble() %>% 
+    mutate(e_plus = V1 + 1,
+           cumulative_equal = e_plus %>% cumprod()) %>% 
+    select(cumulative_equal)
+  
+  returns <- bind_cols(date, as.vector(markowitz), as.vector(cum_markowitz), 
+                       as.vector(interval), as.vector(cum_interval),
+                       as.vector(equal), as.vector(cum_equal))
+  colnames(returns) <- c("date", "markowitz","cumulative_markowitz", 
+                         "interval", "cumulative_interval","equal", "cumulative_equal")
+  
+  return(returns)
+}
